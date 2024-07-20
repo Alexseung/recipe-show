6733ba7e6ed90200db4de2b958b8472586a803fd
Seunggeun Yoon <tmdmrs522@gmail.com>
2024-07-20 22:54:16 +0900

    nav-menu

--- js/common.js
+++ js/common.js
@@ -1,17 +1,16 @@
 // 웹페이지 메인화면에 대한 자바스크립트
 
-let menus = document.querySelectorAll('div.nav-menu div');
-menus.forEach(menu => menu.addEventListener('click', e => showCheckBox(e)));
-let previousCategory = null;
+let navMenus = document.querySelectorAll('nav.nav-menu a');
+navMenus.forEach(item => item.addEventListener('click', e => showNavMenu(e)));
+let previousNavCategory = null;
 
-function showCheckBox(e) {
-  const currentCategory = e.currentTarget;
-  console.log(currentCategory, currentCategory.textContent);
-  //   if (previousCategory) {
-  //     previousCategory.classList.remove('active-nav');
-  //     s;
-  //   }
-  //   // 칼로리, 지방, 나트륨, 단백질의 체크박스 none, flex
-  //   currentCategory.classList.add('active-nav');
-  //   previousCategory = currentCategory;
+function showNavMenu(e) {
+  let currentNavCategory = e.currentTarget;
+  console.log(currentNavCategory, currentNavCategory.textContent);
+
+  if (previousNavCategory) {
+    previousNavCategory.classList.remove('active-nav');
+  }
+  currentNavCategory.classList.add('active-nav');
+  previousNavCategory = currentNavCategory;
 }
